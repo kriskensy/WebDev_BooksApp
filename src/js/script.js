12a34c1926743dc8e69b043a6cfda8cf740f81e9
kris.kensy <kriskensy@proton.me>
2025-03-18 21:25:54 +0100

    Add initActions function with mark book as afvorite option

--- src/js/script.js
+++ src/js/script.js
@@ -15,14 +15,38 @@
 
     //for every book from dataSource.books
     for(const book of dataSource.books){
-        //generate html from data and template
-        const bookHTML = templateBook(book);
-        //create DOM from html
-        const bookElementDOM = utils.createDOMFromHTML(bookHTML);
-        //add generated DOM as child to bookList
-        booksList.appendChild(bookElementDOM);
+      //generate html from data and template
+      const bookHTML = templateBook(book);
+      //create DOM from html
+      const bookElementDOM = utils.createDOMFromHTML(bookHTML);
+      //add generated DOM as child to bookList
+      booksList.appendChild(bookElementDOM);
     }
-  }
+  };
+
+  const initActions = function(){
+    const favoriteBooks = [];
+    //select all .book__image elements
+    const bookImages = booksList.querySelectorAll('.book__image');
+
+    //for all .book__image elements
+    for(const item of bookImages){
+      item.addEventListener('dblclick', function(event){
+        //prevent default
+        event.preventDefault();
+        //add favorite class to clicked item
+        event.target.classList.add('favorite');
+        //select book id from data-id attributte
+        const bookId = event.target.getAttribute('data-id');
+        //add this id to favoriteBooks
+        if(!favoriteBooks.includes(bookId)){
+          favoriteBooks.push(bookId);
+        }
+        console.log('favoriteBooks: ', favoriteBooks);
+      });
+    }
+  };
 
   renderBooks();
+  initActions();
 }
