7bbdb2c6707e11bd5b1b1f282aec05ecaf06f8b8
kris.kensy <kriskensy@proton.me>
2025-03-24 20:36:45 +0100

    Change initActions to use event delegation

--- src/js/script.js
+++ src/js/script.js
@@ -26,33 +26,60 @@
 
   const initActions = function(){
     const favoriteBooks = [];
-    //select all .book__image elements
-    const bookImages = booksList.querySelectorAll('.book__image');
 
-    //for all .book__image elements
-    for(const item of bookImages){
-      item.addEventListener('dblclick', function(event){
-        //prevent default
+    booksList.addEventListener('dblclick', function(event){
+      // const bookImage = event.target.offsetParent.classList.contains('.book__image');
+      const bookImage = event.target.closest('.book__image');
+
+      if(bookImage){
         event.preventDefault();
         //select book id from data-id attributte
-        const bookId = item.getAttribute('data-id');
-
+        const bookId = bookImage.getAttribute('data-id');
+  
         if(!favoriteBooks.includes(bookId)){
-        //add favorite class to clicked item
-        item.classList.add('favorite');
-        //add this id to favoriteBooks
-        favoriteBooks.push(bookId);
+          //add favorite class to clicked item
+          bookImage.classList.add('favorite');
+          //add this id to favoriteBooks
+          favoriteBooks.push(bookId);
         } else {
-        //remove favorite class from clicked item
-        item.classList.remove('favorite');
-        //remove this id from favoriteBooks
-        favoriteBooks.pop(bookId);
+          //remove favorite class from clicked item
+          bookImage.classList.remove('favorite');
+          //remove this id from favoriteBooks
+          favoriteBooks.pop(bookId);
         }
-        console.log('item: ', item);
+        console.log('bookImage: ', bookImage);
         console.log('event.target: ', event.target);
         console.log('favoriteBooks: ', favoriteBooks);
-      });
-    }
+      }
+    });
+
+
+    // //select all .book__image elements
+    // const bookImages = booksList.querySelectorAll('.book__image');
+    // //for all .book__image elements
+    // for(const item of bookImages){
+    //   item.addEventListener('dblclick', function(event){
+    //     //prevent default
+    //     event.preventDefault();
+    //     //select book id from data-id attributte
+    //     const bookId = item.getAttribute('data-id');
+
+    //     if(!favoriteBooks.includes(bookId)){
+    //     //add favorite class to clicked item
+    //     item.classList.add('favorite');
+    //     //add this id to favoriteBooks
+    //     favoriteBooks.push(bookId);
+    //     } else {
+    //     //remove favorite class from clicked item
+    //     item.classList.remove('favorite');
+    //     //remove this id from favoriteBooks
+    //     favoriteBooks.pop(bookId);
+    //     }
+    //     console.log('item: ', item);
+    //     console.log('event.target: ', event.target);
+    //     console.log('favoriteBooks: ', favoriteBooks);
+    //   });
+    // }
   };
 
   renderBooks();
