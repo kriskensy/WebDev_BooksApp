5fa012b98654cb5846df08524516411b50ad22e1
kris.kensy <kriskensy@proton.me>
2025-03-25 19:29:22 +0100

    Add filters array and listener to filtersList to use it with event delegation, for filter books.

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,9 @@
   //compile template
   const templateBook = Handlebars.compile(templateBookSource);
 
+  const filters = [];
+  const filtersList = document.querySelector('.filters');
+
   //create render function
   const renderBooks = function(){
     //clear booksList
@@ -45,41 +48,35 @@
           //remove favorite class from clicked item
           bookImage.classList.remove('favorite');
           //remove this id from favoriteBooks
-          favoriteBooks.pop(bookId);
+          const index = favoriteBooks.indexOf(bookImage.value);
+          if(index !== -1){
+            favoriteBooks.splice(index, 1);
+          }
         }
-        console.log('bookImage: ', bookImage);
-        console.log('event.target: ', event.target);
-        console.log('favoriteBooks: ', favoriteBooks);
       }
     });
 
+    filtersList.addEventListener('click', function(event){
+      //prepare condition
+      const filterCondition = event.target.tagName === 'INPUT' && event.target.type === 'checkbox' && event.target.name === 'filter';
 
-    // //select all .book__image elements
-    // const bookImages = booksList.querySelectorAll('.book__image');
-    // //for all .book__image elements
-    // for(const item of bookImages){
-    //   item.addEventListener('dblclick', function(event){
-    //     //prevent default
-    //     event.preventDefault();
-    //     //select book id from data-id attributte
-    //     const bookId = item.getAttribute('data-id');
+      if(filterCondition){
+        console.log('event.target.value: ', event.target.value);
 
-    //     if(!favoriteBooks.includes(bookId)){
-    //     //add favorite class to clicked item
-    //     item.classList.add('favorite');
-    //     //add this id to favoriteBooks
-    //     favoriteBooks.push(bookId);
-    //     } else {
-    //     //remove favorite class from clicked item
-    //     item.classList.remove('favorite');
-    //     //remove this id from favoriteBooks
-    //     favoriteBooks.pop(bookId);
-    //     }
-    //     console.log('item: ', item);
-    //     console.log('event.target: ', event.target);
-    //     console.log('favoriteBooks: ', favoriteBooks);
-    //   });
-    // }
+        //check checkbox value
+        if(event.target.checked){
+          filters.push(event.target.value);
+        } else{
+          //find index of value to remove
+          const index = filters.indexOf(event.target.value);
+          //remove it
+          if(index !== -1){
+            filters.splice(index, 1);
+          }
+        }
+      }
+      console.log('filters: ', filters);
+    });
   };
 
   renderBooks();
